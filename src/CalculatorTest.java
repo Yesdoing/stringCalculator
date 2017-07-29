e3b5af6df5747426bb76b6f10f026f91dbba0420
yesdoing <fkrmtleo12@gmail.com>
2017-07-29 15:34:39 +0900

    테스트 코드 메소드 별 분

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -2,9 +2,32 @@
 public class CalculatorTest {
 	public static void main(String[] args) {
 		Calculator cal = new Calculator();
-		System.out.println(cal.add(3, 4));
-		System.out.println(cal.subtract(5, 4));
-		System.out.println(cal.multiply(2, 6));
-		System.out.println(cal.divide(8, 4));
+		add(cal);
+		subtract(cal);
+		multiply(cal);
+		divide(cal);
 	}
+
+	private static void divide(Calculator cal) {
+		// TODO Auto-generated method stub
+		System.out.println(cal.divide(9, 3));
+	}
+
+	private static void multiply(Calculator cal) {
+		// TODO Auto-generated method stub
+		System.out.println(cal.multiply(3, 4));
+	}
+
+	private static void subtract(Calculator cal) {
+		// TODO Auto-generated method stub
+		System.out.println(cal.subtract(2, 1));
+	}
+
+	private static void add(Calculator cal) {
+		// TODO Auto-generated method stub
+		System.out.println(cal.add(5, 4));
+	}
+	
+	
+	
 }
