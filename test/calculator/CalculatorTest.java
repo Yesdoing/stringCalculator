da035cb9fdc868289aaf3ff931b5c5da4fbd5199
yesdoing <fkrmtleo12@gmail.com>
2017-07-29 16:07:25 +0900

    단위 테스트 코드 리펙토링

--- test/calculator/CalculatorTest.java
+++ test/calculator/CalculatorTest.java
@@ -11,15 +11,13 @@ public class CalculatorTest {
 	@Test
 	public void add() {
 		Calculator cal = new Calculator();
-		int result = cal.add(2, 1);
-		assertEquals(3, result);
+		assertEquals(3, cal.add(2, 1));
 	}
 
 	@Test
 	public void divide() {
 		Calculator cal = new Calculator();
-		int result = cal.divide(9, 3);
-		assertEquals(3, result);
+		assertEquals(3, cal.divide(9, 3));
 	}
 	
 	
