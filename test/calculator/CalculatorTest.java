7e9cb7bdc29963f226e40d7b0cbed9dad9ee2366
yesdoing <fkrmtleo12@gmail.com>
2017-07-29 16:13:41 +0900

    After 애노테이션 사용

--- test/calculator/CalculatorTest.java
+++ test/calculator/CalculatorTest.java
@@ -2,6 +2,7 @@ package calculator;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,16 +14,24 @@ public class CalculatorTest {
 	@Before
 	public void setup() {
 		cal = new Calculator();
+		System.out.println("setup!");
 	}
 
 	@Test
 	public void add() {
 		assertEquals(3, cal.add(2, 1));
+		System.out.println("add!");
 	}
 
 	@Test
 	public void divide() {
 		assertEquals(3, cal.divide(9, 3));
+		System.out.println("divide!");
+	}
+	
+	@After
+	public void teardown() {
+		System.out.println("teardown!");
 	}
 	
 	
