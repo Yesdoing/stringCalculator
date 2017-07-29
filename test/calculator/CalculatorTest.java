e906fe7a42b7471383affe7626e6eb7c9fd19257
yesdoing <fkrmtleo12@gmail.com>
2017-07-29 16:08:48 +0900

    테스트 코드 리펙토링 - Calculator 객체 전역변수로 변

--- test/calculator/CalculatorTest.java
+++ test/calculator/CalculatorTest.java
@@ -7,16 +7,15 @@ import org.junit.Test;
 import calculator.Calculator;
 
 public class CalculatorTest {
+	private Calculator cal = new Calculator();
 
 	@Test
 	public void add() {
-		Calculator cal = new Calculator();
 		assertEquals(3, cal.add(2, 1));
 	}
 
 	@Test
 	public void divide() {
-		Calculator cal = new Calculator();
 		assertEquals(3, cal.divide(9, 3));
 	}
 	
